2b1c2f5c1fdba28e95c4496c8854b9311450c20c
Andre Mesquita <avmesquita@gmail.com>
2023-09-29 14:33:25 +0100

    Data lida a partir de ficheiro

--- src/modules/main/services/contagem-regressiva.service.ts
+++ src/modules/main/services/contagem-regressiva.service.ts
@@ -1,46 +1,39 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { ContagemRegressivaModel } from '../models/contagem-regressiva.model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContagemRegressivaService implements OnDestroy {
-  private count_down: number;
-  private timer: NodeJS.Timer;
+
+  private count_down: number = 0;
+  private timer?: NodeJS.Timer;
 
   public app = new ContagemRegressivaModel();
 
-  //H.PT = 23
-  private dataPrimaria = new Date('06/05/2023 19:00:00');   
-  //H.PT = 13
-  private dataSecundaria = new Date('01/31/2024 17:00:00');
+  private fromDate = new Date;
+  private dates: any;
 
-  private fromDate: Date = this.dataPrimaria;
+  constructor(protected http: HttpClient,
+              private window: Window) {
 
-  constructor(private window: Window) {
-    let now = new Date(Date.now()).getTime();    
-    const evt = {
-      event_category: 'query_date',
-      event_label: 'setQueryDate',
-      value: new Date(Date.now()).toDateString()
-    };
-    (<any>this.window).gtag('event', 'query_date', evt);
-
-    if (now < this.dataPrimaria.getTime()) {
-      this.fromDate = this.dataPrimaria;
-    } else {
-      this.fromDate = this.dataSecundaria;
-    }
-    this.app = new ContagemRegressivaModel();
-    
-    this.count_down = this.fromDate ? this.fromDate.getTime() : 0;
-    this.timer = setInterval(() => this.countDown(), 1);
-
+    this.getDataset().subscribe( (response: any) => {
+      this.dates = response;
+      const aData = new Date(this.dates[0].data);      
+      this.fromDate = aData;
+      this.app = new ContagemRegressivaModel();
+      
+      this.count_down = this.fromDate ? this.fromDate.getTime() : 0;
+      this.timer = setInterval(() => this.countDown(), 1);
+    });
     console.log('Tem Easter Egg para o Miguel e para a Julia!');
   }
 
   ngOnDestroy(): void {
-    this.timer.unref();
+    if (this.timer)
+      this.timer.unref();
   }
 
   public setFromDate(date: Date): void {
@@ -55,11 +48,14 @@ export class ContagemRegressivaService implements OnDestroy {
     const minute = second * 60;
     const hour = minute * 60;    
     const day = hour * 24;
-
     
     this.app.days = Math.floor(diff / day);
     this.app.hours = Math.floor(diff % day / hour);
     this.app.minutes = Math.floor(diff % hour / minute);
     this.app.seconds = Math.floor(diff % minute / second);
-  }  
+  } 
+  
+  getDataset(): Observable<any> {
+    return this.http.get<any>('/assets/datasets/dates.json', { observe: 'body' });
+  }
 }
